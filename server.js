4422c161adbc61c543728c073d2603b80d710092
Girish Bedekar <gbedekar@adobe.com>
2026-05-06 15:12:38 -0700

    Update server.js

--- server.js
+++ server.js
@@ -191,6 +191,16 @@ app.get("/users/:userId/orders", (req, res) => {
   return res.json({ orders });
 });
 
+app.post("/submit", (req, res) => {
+  console.log("Form submission received:");
+  console.log(req.body);
+
+  res.status(200).json({
+    success: true,
+    message: "Submission received"
+  });
+});
+
 /**
  * POST /generate-pdf-base64
  * Body: { "userId": "12345" }
